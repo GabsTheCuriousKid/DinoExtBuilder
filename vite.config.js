4f1691dc37a3dc45c7ff8e87dce229ce07e55202
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-09-05 15:43:28 +0200

    Initial Commit
    dinoExtBuilder stuff (NOT FINISHED)

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [
+		sveltekit()
+	],
+	optimizeDeps: {
+		include: [
+			'@blockly/continuous-toolbox',
+			'file-saver',
+		]
+	},
+	build: {
+		sourcemap: true
+	}
+});
